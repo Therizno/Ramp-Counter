bb96efce383c5a5b099c0a3c7bccd5aad11ba3e0
William Pembleton <william.pembleton17@ncf.edu>
2019-02-26 17:01:28 -0500

    Java FX

---
+++ src/sample/Launcher.java
@@ -0,0 +1,12 @@
+import sample.RampCounter;
+
+/**
+ * Launcher which serves as an entry point to the app.
+ * In order to get a working, assembled program, we can NOT have the main class extend from Application.
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        RampCounter.main(args);
+    }
+}
